bb6dc91c59ec4c23de472f09d3921846b66462a3
Weston Ruter <weston@xwp.co>
2018-04-15 12:57:31 -0700

    Fix majority of phpcs complaints

--- jsforwp-how-to-gutenberg.php
+++ jsforwp-how-to-gutenberg.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Gutenberg - How to Register a Block
  * Plugin URI: https://gutenberg.courses
- * Description: An plugin for learing how Gutenberg blocks work.  From <a href="https://gutenberg.courses">Zac Gordon's Gutenberg Course</a>.
+ * Description: An plugin for learning how Gutenberg blocks work.  From <a href="https://gutenberg.courses">Zac Gordon's Gutenberg Course</a>.
  * Text Domain: jsforwphowto
  * Domain Path: /languages
  * Author: Zac Gordon
@@ -14,133 +14,143 @@
  * @package jsforwphowto
  */
 
-//  Exit if accessed directly.
-defined('ABSPATH') || exit;
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
 
-
+/**
+ * Augment post type with block template.
+ *
+ * @param array  $args      Post type args.
+ * @param string $post_type Post type name.
+ * @return array Post type args.
+ */
 function jsforwphowto_templates( $args, $post_type ) {
 
-  if ( $post_type == 'post' ) {
-    $args['template_lock'] = true;
-    $args['template'] = [
-      [
-        'core/image', [
-          'align' => 'left',
-        ]
-      ],
-      [
-        'core/paragraph', [
-          'placeholder' => 'The only thing you can add',
-        ]
-      ]
-    ];
-  }
+	if ( 'post' === $post_type ) {
+		$args['template_lock'] = true;
+		$args['template']      = array(
+			array(
+				'core/image',
+				array(
+					'align' => 'left',
+				),
+			),
+			array(
+				'core/paragraph',
+				array(
+					'placeholder' => 'The only thing you can add',
+				),
+			),
+		);
+	}
 
-  return $args;
-
+	return $args;
 }
-//add_filter( 'register_post_type_args', 'jsforwphowto_templates', 20, 2 );
+// add_filter( 'register_post_type_args', 'jsforwphowto_templates', 20, 2 );
 
 /**
  * Enqueue block editor only JavaScript and CSS
  */
-function jsforwphowto_editor_scripts()
-{
+function jsforwphowto_editor_scripts() {
+	// Make paths variables so we don't write em twice ;).
+	$block_path        = '/assets/js/editor.blocks.js';
+	$editor_style_path = '/assets/css/blocks.editor.css';
 
-    // Make paths variables so we don't write em twice ;)
-    $blockPath = '/assets/js/editor.blocks.js';
-    $editorStylePath = '/assets/css/blocks.editor.css';
+	// Enqueue the bundled block JS file.
+	wp_enqueue_script(
+		'jsforwp-blocks-js',
+		plugins_url( $block_path, __FILE__ ),
+		array( 'wp-i18n', 'wp-element', 'wp-blocks', 'wp-components', 'wp-api' ),
+		filemtime( plugin_dir_path( __FILE__ ) . $block_path )
+	);
 
-    // Enqueue the bundled block JS file
-    wp_enqueue_script(
-        'jsforwp-blocks-js',
-        plugins_url( $blockPath, __FILE__ ),
-        [ 'wp-i18n', 'wp-element', 'wp-blocks', 'wp-components', 'wp-api' ],
-        filemtime( plugin_dir_path(__FILE__) . $blockPath )
-    );
-
-    // Enqueue optional editor only styles
-    wp_enqueue_style(
-        'jsforwp-blocks-editor-css',
-        plugins_url( $editorStylePath, __FILE__),
-        [ 'wp-blocks' ],
-        filemtime( plugin_dir_path( __FILE__ ) . $editorStylePath )
-    );
+	// Enqueue optional editor only styles.
+	wp_enqueue_style(
+		'jsforwp-blocks-editor-css',
+		plugins_url( $editor_style_path, __FILE__ ),
+		array( 'wp-blocks' ),
+		filemtime( plugin_dir_path( __FILE__ ) . $editor_style_path )
+	);
 
 }
 
-// Hook scripts function into block editor hook
+// Hook scripts function into block editor hook.
 add_action( 'enqueue_block_editor_assets', 'jsforwphowto_editor_scripts' );
 
 
 /**
  * Enqueue front end and editor JavaScript and CSS
  */
-function jsforwphowto_scripts()
-{
-    $blockPath = '/assets/js/frontend.blocks.js';
-    // Make paths variables so we don't write em twice ;)
-    $stylePath = '/assets/css/blocks.style.css';
+function jsforwphowto_scripts() {
+	// Make paths variables so we don't write em twice ;).
+	$block_path = '/assets/js/frontend.blocks.js';
+	$style_path = '/assets/css/blocks.style.css';
 
-    // Enqueue the bundled block JS file
-    wp_enqueue_script(
-        'jsforwp-blocks-frontend-js',
-        plugins_url( $blockPath, __FILE__ ),
-        [ 'wp-i18n', 'wp-element', 'wp-blocks', 'wp-components', 'wp-api' ],
-        filemtime( plugin_dir_path(__FILE__) . $blockPath )
-    );
+	// Enqueue the bundled block JS file.
+	wp_enqueue_script(
+		'jsforwp-blocks-frontend-js',
+		plugins_url( $block_path, __FILE__ ),
+		array( 'wp-i18n', 'wp-element', 'wp-blocks', 'wp-components', 'wp-api' ),
+		filemtime( plugin_dir_path( __FILE__ ) . $block_path )
+	);
 
-    // Enqueue frontend and editor block styles
-    wp_enqueue_style(
-        'jsforwp-blocks-css',
-        plugins_url($stylePath, __FILE__),
-        [ 'wp-blocks' ],
-        filemtime(plugin_dir_path(__FILE__) . $stylePath )
-    );
+	// Enqueue frontend and editor block styles.
+	wp_enqueue_style(
+		'jsforwp-blocks-css',
+		plugins_url( $style_path, __FILE__ ),
+		array( 'wp-blocks' ),
+		filemtime( plugin_dir_path( __FILE__ ) . $style_path )
+	);
 
 }
 
-// Hook scripts function into block editor hook
-add_action('enqueue_block_assets', 'jsforwphowto_scripts');
+// Hook scripts function into block editor hook.
+add_action( 'enqueue_block_assets', 'jsforwphowto_scripts' );
 
 
 /**
  * Server rendering for /blocks/examples/12-dynamic
+ *
+ * @param array $attributes Block attributes.
+ * @return string Rendered block.
  */
 function jsforwp_dynamic_block_render( $attributes ) {
 
-    $recent_posts = wp_get_recent_posts( [
-        'numberposts' => 1,
-        'post_status' => 'publish',
-    ] );
-    if ( count( $recent_posts ) === 0 ) {
-        return 'No posts';
-    }
-    $post = $recent_posts[ 0 ];
-    $post_id = $post['ID'];
-    return sprintf(
-        '<p><a class="wp-block-my-plugin-latest-post" href="%1$s">%2$s</a></p>',
-        esc_url( get_permalink( $post_id ) ),
-        esc_html( get_the_title( $post_id ) )
-    );
+	$recent_posts = wp_get_recent_posts(
+		array(
+			'numberposts' => 1,
+			'post_status' => 'publish',
+		)
+	);
+	if ( count( $recent_posts ) === 0 ) {
+		return 'No posts';
+	}
+	$post    = $recent_posts[0];
+	$post_id = $post['ID'];
+	return sprintf(
+		'<p><a class="wp-block-my-plugin-latest-post" href="%1$s">%2$s</a></p>',
+		esc_url( get_permalink( $post_id ) ),
+		esc_html( get_the_title( $post_id ) )
+	);
 
 }
 
-// Hook server side rendering into render callback
-register_block_type( 'jsforwp/dynamic', [
-    'render_callback' => 'jsforwp_dynamic_block_render',
-] );
-
+// Hook server side rendering into render callback.
+register_block_type(
+	'jsforwp/dynamic', array(
+		'render_callback' => 'jsforwp_dynamic_block_render',
+	)
+);
 
 /**
  * Registering meta fields for block attributes that use meta storage
  */
 function jsforwp_register_meta() {
-    $args = array(
-        'type' => 'string',
-        'single' => true,
-        'show_in_rest' => true,
-    );
-    register_meta( 'post', 'jsforwp_gb_metabox', $args );
+	$args = array(
+		'type'         => 'string',
+		'single'       => true,
+		'show_in_rest' => true,
+	);
+	register_meta( 'post', 'jsforwp_gb_metabox', $args );
 }
-add_action('init', 'jsforwp_register_meta');
+add_action( 'init', 'jsforwp_register_meta' );
